10921264101adfd036fd0fc67c31a349c7445563
Barbara Bruzon (BR) <105392656+barbara-bruzon@users.noreply.github.com>
2023-09-16 14:40:12 -0300

    sucessor e antecessor de um num

---
+++ aula-03/sucessor-antecessor.cpp
@@ -0,0 +1,16 @@
+/*Estrutura de Dados: Exercício 01 - Antecessor e Sucessor - 02.09.2023
+Enunciado 01. Ler um número inteiro e imprimir seu sucessor e seu antecessor*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int num;
+	
+	printf("Insira um número inteiro qualquer: ");
+	scanf("%d", &num);
+	printf("\n\n\nO antecessor desse número é %d.\nO sucessor desse número é %d", num-1, num+1);
+	return 0;
+}
