4e173e49369fccf5b4b003b919f0ae61f1b016d2
Barbara Bruzon (BR) <105392656+barbara-bruzon@users.noreply.github.com>
2023-09-16 14:41:20 -0300

    fixing date

--- aula-03/sucessor-antecessor.cpp
+++ aula-03/sucessor-antecessor.cpp
@@ -1,4 +1,4 @@
-/*Estrutura de Dados: Exercício 01 - Antecessor e Sucessor - 02.09.2023
+/*Estrutura de Dados: Exercício 01 - Antecessor e Sucessor - 26.08.2023
 Enunciado 01. Ler um número inteiro e imprimir seu sucessor e seu antecessor*/
 
 #include <stdio.h>
